22d581394fdce9cabaa3ad9b65b6846385287f49
Emre <ekocausa@gmail.com>
2015-10-12 23:08:53 -0700

    Client app

--- src/com/emrekoca/client/SimpleClientApp.java
+++ src/com/emrekoca/client/SimpleClientApp.java
@@ -10,6 +10,7 @@ import com.emrekoca.domain.Action;
 import com.emrekoca.domain.Call;
 import com.emrekoca.services.calls.CallHandlingService;
 import com.emrekoca.services.customers.CustomerNotFoundException;
+import com.emrekoca.services.diary.DiaryManagementService;
 
 public class SimpleClientApp {
 
@@ -20,7 +21,7 @@ public class SimpleClientApp {
 		//DiaryManagementService diaryService = container.getBean(DiaryManagementService.class);
 		CallHandlingService callService = container.getBean(CallHandlingService.class);
 		Action action1 = new Action("Call back me!", new GregorianCalendar(2016, 0, 0), "ek");
-		Action action2 = new Action("I hate you!", new GregorianCalendar(2018, 0, 0), "ek2");
+		Action action2 = new Action("I hate you!", new GregorianCalendar(2018, 0, 0), "ek");
 		List<Action> list = new ArrayList<Action>();
 		list.add(action1);
 		list.add(action2);
@@ -28,6 +29,11 @@ public class SimpleClientApp {
 			callService.recordCall("AB123", new Call("Test testing"), list);
 		} catch (CustomerNotFoundException e) {
 			e.printStackTrace();
+		}
+		DiaryManagementService diaryService = container.getBean(DiaryManagementService.class);
+		for(Action action : diaryService.getAllIncompleteActions("ek"))
+		{
+			System.out.println(action);
 		}
 		container.close();
 	}
