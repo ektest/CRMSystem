fc7593e45607b836a73ec7614a9337e4691a29d6
Emre <ekocausa@gmail.com>
2015-10-12 22:59:31 -0700

    Client app

--- src/com/emrekoca/client/SimpleClientApp.java
+++ src/com/emrekoca/client/SimpleClientApp.java
@@ -1,21 +1,34 @@
 package com.emrekoca.client;
 
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.emrekoca.domain.Customer;
-import com.emrekoca.services.customers.CustomerManagementService;
-import com.emrekoca.services.diary.DiaryManagementService;
+import com.emrekoca.domain.Action;
+import com.emrekoca.domain.Call;
+import com.emrekoca.services.calls.CallHandlingService;
+import com.emrekoca.services.customers.CustomerNotFoundException;
 
 public class SimpleClientApp {
 
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext container = new ClassPathXmlApplicationContext("application.xml");
-		CustomerManagementService customerService = container.getBean("customerService", CustomerManagementService.class);
-		for(Customer next : customerService.getAllCustomers()){
-			System.out.println(next);
-		}
+		//CustomerManagementService customerService = container.getBean("customerService", CustomerManagementService.class);
 		// Another way to wire bean if there is only one configuration of that class in application.xmll
-		DiaryManagementService diaryService = container.getBean(DiaryManagementService.class);
+		//DiaryManagementService diaryService = container.getBean(DiaryManagementService.class);
+		CallHandlingService callService = container.getBean(CallHandlingService.class);
+		Action action1 = new Action("Call back me!", new GregorianCalendar(2016, 0, 0), "ek");
+		Action action2 = new Action("I hate you!", new GregorianCalendar(2018, 0, 0), "ek2");
+		List<Action> list = new ArrayList<Action>();
+		list.add(action1);
+		list.add(action2);
+		try {
+			callService.recordCall("AB123", new Call("Test testing"), list);
+		} catch (CustomerNotFoundException e) {
+			e.printStackTrace();
+		}
 		container.close();
 	}
 
